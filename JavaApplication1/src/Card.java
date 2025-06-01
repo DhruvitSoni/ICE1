c831bb36f12c08168c75b7954550d4d597ed995f
dbs28 <dhruvitsoni96@gmail.com>
2025-06-01 00:34:55 -0400

    Complete ICE1

---
+++ JavaApplication1/src/Card.java
@@ -0,0 +1,24 @@
+/**
+ * @ DhruvitSoni, 991809845
+ * @date 2025-05-31
+ */
+public class Card {
+     private String suit;
+    private int value;
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
---
+++ JavaApplication1/src/CardTrick.java
@@ -0,0 +1,41 @@
+
+/**
+ * @ DhruvitSoni, 991809845
+ * @date 2025-05-31
+ */
+import java.util.Random;
+public class CardTrick {
+    public static void main(String[] args) {
+        Card[] magicHand = new Card[7];
+        String[] suits = {"Hearts", "Diamonds", "Spades", "Clubs"};
+
+        Random rand = new Random();
+
+        for (int i = 0; i < magicHand.length; i++) {
+            Card c = new Card();
+            c.setValue(rand.nextInt(13) + 1); 
+            c.setSuit(suits[rand.nextInt(4)]); 
+            magicHand[i] = c;
+        }
+        Card luckyCard = new Card();
+        luckyCard.setValue(7);
+        luckyCard.setSuit("Hearts");
+        boolean found = false;
+        for (Card card : magicHand) {
+            if (card.getValue() == luckyCard.getValue() && card.getSuit().equalsIgnoreCase(luckyCard.getSuit())) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            System.out.println("🎉 Congratulations!");
+        } else {
+            System.out.println("Sorry!");
+        }
+        System.out.println("\nMagic Hand:");
+        for (Card card : magicHand) {
+            System.out.println(card.getValue() + " of " + card.getSuit());
+        }
+        System.out.println("\nLucky Card: " + luckyCard.getValue() + " of " + luckyCard.getSuit());
+    }
+}
